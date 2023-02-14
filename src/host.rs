08999af79dd4037f66f2ea759f237e2be68f2daa
anylots <842567494@qq.com>
2023-02-14 20:33:05 +0800

    host

---
+++ src/host.rs
@@ -0,0 +1,11 @@
+use crate::interpreter;
+use crate::state::State;
+use crate::storage::spec::Database;
+use crate::types::{Env, RunResult};
+
+
+//get block number
+pub fn get_blockNum() -> uint256 {}
+
+//get block timestamp
+pub fn get_blockTs() -> Date {}
